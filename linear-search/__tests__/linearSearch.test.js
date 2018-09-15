c291452a7fd76a279b53ecd645cd65a3c6f0d612
Ridge Kimani <ridgekimani@gmail.com>
2018-09-15 20:28:07 +0300

    remove space

--- linear-search/__tests__/linearSearch.test.js
+++ linear-search/__tests__/linearSearch.test.js
@@ -2,7 +2,6 @@ const linearSearch = require("../linearSearch")
 
 const testArray = [1, 2, 3, 4, 5, 6]
 
-
 describe ("linear search tests", () => {
     test('it returns a specific index on search', () => {
         expect(linearSearch(testArray, 1)).toBe(0)
